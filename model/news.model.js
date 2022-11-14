bdb69ce10b2fe00adbc3513e715ccb459dd27370
nesy <umoizeonesi@gmail.com>
2022-11-14 17:09:08 +0000

    get api/topics done and error handled

--- model/news.model.js
+++ model/news.model.js
@@ -1,7 +1,7 @@
 const db = require("../db/connection");
 
 exports.selectedTopics = () => {
-  return db.query("SELECT * FROM topics").then((res) => {
-    return res.rows;
+  return db.query("SELECT * FROM topics").then((response) => {
+    return response.rows;
   });
 };
