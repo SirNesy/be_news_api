2c8995f05a45d3c67cbce5e7955251d0c33f10a3
nesy <umoizeonesi@gmail.com>
2022-11-15 20:47:59 +0000

    from main

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+const { application } = require("express");
 const express = require("express");
-const { handlePSQLErrors } = require("./errors");
-const { getTopics } = require("./controller/news.controller");
+const { getTopics, getArticles } = require("./controller/news.controller");
 const app = express();
 
 app.get("/api/topics", getTopics);
 
+app.get("/api/articles", getArticles);
+
 app.all("/*", (req, res) => {
   res.status(404).send({ msg: "invalid URL!" });
+});
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  }
+});
+app.use((err, req, res, next) => {
+  res.status(500).send({ msg: "Server error!" });
 });
 
 module.exports = app;
